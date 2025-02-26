d53ab2d34c6feb23f67e23f25d5be27cda662daa
Raj-Coding-World9026 <106828701+Raj-Coding-World9026@users.noreply.github.com>
2025-02-26 17:13:46 +0530

    Update UserProcessor.java

--- SpringBatch/SpringBatch/src/main/java/springBatch/UserProcessor.java
+++ SpringBatch/SpringBatch/src/main/java/springBatch/UserProcessor.java
@@ -9,10 +9,8 @@ public class UserProcessor implements ItemProcessor<User, User> {
 
     @Override
     public User process(User user) throws Exception{
-        System.out.println("p enter");
 
         user.setEmail(user.getEmail().toLowerCase());
-        System.out.println("🔄 Processing User: " + user); // Convert email to lowercase
         return user;
     }
 
